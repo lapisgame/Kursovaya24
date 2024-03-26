768fcf4483a563c5465c5b626cbd370a3e9524e6
lapisgame <lapisgames5@gmail.com>
2024-03-26 23:46:52 +0300

    add secondary window prototype

--- qtLlearn/main.py
+++ qtLlearn/main.py
@@ -1,7 +1,16 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget
 from MainWindow import Ui_MainWindow  # импорт нашего сгенерированного файла9
 import sys
  
+class SecondaryWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Второе окно')
+        # Здесь можно добавить дополнительные виджеты и настройки для второго окна
+
+    def closeEvent(self, event):
+        self.parent().show()  # При закрытии второго окна основное окно становится активным
+        super().closeEvent(event)
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
@@ -15,7 +24,11 @@ class MainWindow(QMainWindow):
         self.ui.AboutButton.clicked.connect(self.setupAboutWindow)
 
     def setupLectureWindow(self):
-        pass
+        self.secondary_window = SecondaryWindow()
+        self.secondary_window.setParent(self, self.secondary_window.windowFlags())
+        self.secondary_window.show()
+        self.hide()  # Отключаем основное окно
+
 
     def setupTaskWindow(self):
         pass
@@ -28,6 +41,19 @@ class MainWindow(QMainWindow):
 
     def setupAboutWindow(self):
         pass
+        
+
+
+class NewWindow(QMainWindow):
+    def __init__(self, main_window):
+        super().__init__()
+        self.setWindowTitle("Новое окно")
+        self.main_window = main_window
+
+    def closeEvent(self, event):
+        # Включаем снова основное окно
+        self.main_window.setEnabled(True)
+        event.accept()
 
     
  
