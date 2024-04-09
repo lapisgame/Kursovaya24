bafd94485ffe8eb80da058417f793530f2c0da5e
Артем Климантов <klimantovartem@gmail.com>
2024-04-09 16:18:11 +0300

    About window preset init

--- qtLlearn/AboutWindow.py
+++ qtLlearn/AboutWindow.py
@@ -1,4 +1,11 @@
 from PyQt6.QtWidgets import QWidget
 
 class AboutWindow(QWidget):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle('О приложении')
+        self.setFixedSize(1150, 800)
+
+    def closeEvent(self, event):
+        self.parent().show()
+        super().closeEvent(event)
