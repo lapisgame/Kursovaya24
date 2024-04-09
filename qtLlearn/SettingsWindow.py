619c789f120b90092e7d9a042c6cb659ebd61ebf
Артем Климантов <klimantovartem@gmail.com>
2024-04-09 16:19:20 +0300

    Settings window preset init

--- qtLlearn/SettingsWindow.py
+++ qtLlearn/SettingsWindow.py
@@ -1,4 +1,11 @@
 from PyQt6.QtWidgets import QWidget
 
 class SettingsWindow(QWidget):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle('Настройки')
+        self.setFixedSize(1150, 800)
+
+    def closeEvent(self, event):
+        self.parent().show()
+        super().closeEvent(event)
