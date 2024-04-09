e932e6218a542c0fcf64b364655b9365a021bff2
Артем Климантов <klimantovartem@gmail.com>
2024-04-09 16:19:29 +0300

    Task window preset init

--- qtLlearn/TaskWindow.py
+++ qtLlearn/TaskWindow.py
@@ -1,4 +1,11 @@
 from PyQt6.QtWidgets import QWidget
 
 class TaskWindow(QWidget):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle('Задачи')
+        self.setFixedSize(1150, 800)
+
+    def closeEvent(self, event):
+        self.parent().show()
+        super().closeEvent(event)
