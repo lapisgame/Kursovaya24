e1d3037a274dc7055de69182cc64e6f09f96e4f5
lapisgame <lapisgames5@gmail.com>
2024-04-16 00:37:02 +0300

    add load from json fontSize

--- qtLlearn/LectureWindow.py
+++ qtLlearn/LectureWindow.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QGroupBox, QTextBrowser, QPushButton, QVBoxLayout, QSpacerItem, QSizePolicy
+from PyQt6.QtWidgets import QWidget, QGroupBox, QLabel, QTextBrowser, QPushButton, QVBoxLayout, QSpacerItem, QSizePolicy
+from PyQt6.QtGui import QTextOption
 
 import json
 import os
@@ -9,12 +10,15 @@ class LectureWindow(QWidget):
         self.setWindowTitle('Лекции')
         self.setFixedSize(1150, 800)
 
+        self.fontSize = self.getFontJson()
+
         self.groupBox = QGroupBox('Выбор Лекции', self)
         self.groupBox.setGeometry(10, 0, 200, 790)
 
         self.initLectureButtons()
 
         self.plainTextEdit = QTextBrowser(self)
+        self.plainTextEdit.setWordWrapMode(QTextOption.WrapMode.WordWrap)
         self.plainTextEdit.setGeometry(210, 10, 930, 780)
 
     def closeEvent(self, event):
@@ -55,22 +59,37 @@ class LectureWindow(QWidget):
 
         self.groupBox.setLayout(layout)
     
+    def getFontJson(self):
+        config_file_path = 'config.json'
+        try:
+            with open(config_file_path, 'r') as config_file:
+                config = json.load(config_file)
+                font_size = config.get('fontSize')
+                return font_size
+        except FileNotFoundError:
+            print(f"Файл {config_file_path} не найден.")
+        except json.JSONDecodeError:
+            print(f"Ошибка при разборе JSON в файле {config_file_path}.")
+
+
     def openLecture(self, path):
         f = open(path, 'r', encoding='utf-8')
         data = json.load(f)
 
-        fontsize = 14
         style = """<style>
                         * {
-                            font-size:" + str(fontsize) + "pt
+                            font-size:""" + str(self.fontSize) + """pt;
                         } 
                         pre {
-                            margin:0;
-                            padding:0;
-                            line-height: 0.8
+                            line-height: 1.25;
+                            max-width: 760 px;
+                            font-size:""" + str(self.fontSize) + """pt;
+                            max-width: 760px;
+                            white-space: pre-wrap;
+                            word-wrap: break-word;
+                            overflow-x: auto;
                         }
                 </style>"""
     
         content = f"{style}{data['content']}"
-        # Установка HTML-контента
         self.plainTextEdit.setHtml(content)
